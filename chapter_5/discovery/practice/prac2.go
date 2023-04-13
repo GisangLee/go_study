6a1edce55b1de6c99b326033018286c3e434a2d5
Choe Beom Kwon <bigy@vitasoft.co.kr>
2023-04-13 16:28:37 +0900

    연습문제 2

--- chapter_5/discovery/practice/prac2.go
+++ chapter_5/discovery/practice/prac2.go
@@ -1,8 +1,63 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sort"
+	"strings"
+)
+
+type CaseInsensitive []string
+
+// 배열 크기
+func (c CaseInsensitive) Len() int {
+	return len(c)
+}
+
+func (c CaseInsensitive) Less(i, j int) bool {
+	return strings.ToLower(c[i]) < strings.ToLower(c[j]) || (strings.ToLower(c[i]) == strings.ToLower(c[j]) && c[i] < c[j])
+}
+
+// Swap swaps the elements with indexes i and j.
+func (c CaseInsensitive) Swap(i, j int) {
+	c[i], c[j] = c[j], c[i]
+}
+
+func (c *CaseInsensitive) Push(x interface{}) {
+	*c = append(*c, x.(string))
+}
+
+func (c *CaseInsensitive) Pop() interface{} {
+	len := c.Len()
+	last := (*c)[len-1]
+	*c = (*c)[:len-1]
+	return last
+}
+
+type Interface interface {
+	sort.Interface
+	Push(x interface{})
+	Pop() interface{}
+}
+
+// 연습문제 2
+// 테이블 기반 테스트
+func TestCaseInsensitiveSort() {
+	cases := []CaseInsensitive{{"iPhone", "iPad", "MacBook", "AppStore"}, {"hi", "bye"}, {"c", "D", "A", "j"}}
+
+	for i, _ := range cases {
+		sort.Sort(cases[i])
+		fmt.Println(cases[i])
+	}
+}
 
 func main() {
 
-	fmt.Println("hi")
+	TestCaseInsensitiveSort()
+	//
+	//heap.Init(&apple)
+	//for apple.Len() > 0 {
+	//	fmt.Println(heap.Pop(&apple))
+	//}
+	//sort.Sort(apple)
+	//fmt.Println(apple)
 }
